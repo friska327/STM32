401c3504daa2a7e72258b2b0c7be50515d52d29e
friska327 <119197021+friska327@users.noreply.github.com>
2022-11-27 22:13:31 +0700

    Delete lab1-2/TIM_example directory

--- lab1-2/TIM_example/interrupt.h
+++
@@ -1,4 +0,0 @@
-void TIM4_IRQHandler ();
-void EXTI0_IRQHandler ();
-void EXTI1_IRQHandler ();
-void SysTick_Handler();
--- lab1-2/TIM_example/my_lib.h
+++
@@ -1,8 +0,0 @@
-
-
-
-void SystemCoreClockConfigure(void);
-void TIM4_Init ();
-void GPIO_Init (void);
-void DELAY_us (int us);
-void ExINT0_PA0();
